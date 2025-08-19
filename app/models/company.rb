ea850b1078544f42a501d1788de9b29b13c39b9f
Jules Baralotto <jules.baralotto@kedgebs.com>
2025-08-19 14:06:48 +0200

    added the navbar, the authentification form and designed it

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,6 +2,6 @@ class Company < ApplicationRecord
   belongs_to :user
   has_many :chats
   validates :name, presence: true, uniqueness: true
-  validates :description
-  validates :objectives
+  validates :description, presence: true
+  validates :objectives, presence: true
 end
