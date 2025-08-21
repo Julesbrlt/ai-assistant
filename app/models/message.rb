e431302f12773e163aba56e73a10181cefee7c38
Jules Baralotto <jules.baralotto@kedgebs.com>
2025-08-21 11:26:05 +0200

    changed db

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,3 @@
 class Message < ApplicationRecord
-  belongs_to :company
+  belongs_to :chat
 end
---
+++ db/migrate/20250821091624_add_message_to_chat.rb
@@ -0,0 +1,5 @@
+class AddMessageToChat < ActiveRecord::Migration[7.1]
+  def change
+    add_reference :messages, :chat, foreign_key: true
+  end
+end
